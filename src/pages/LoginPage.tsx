336b6d083e048b8bfdb84415747cc9944d2b7659
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-03-06 14:38:17 +0100

    Create login form with functionality

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,48 @@
+import { useState, type SyntheticEvent } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 import "./LoginPage.css";
 
 function LoginPage() {
-    return(
-        <>
-        </>
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = async (e: SyntheticEvent<HTMLFormElement>)  => {
+        e.preventDefault();
+        try {
+            await login({ email, password });
+            // Redirect after successful login
+            navigate("/admin"); 
+        } catch (err) {
+            setError("Invalid email or password");
+        }
+    };
+    return (
+        <div>
+        <h2>Login</h2>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     )
 }
 
